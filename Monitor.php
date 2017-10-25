348ceb71c36f3733f68a73112ab5fd37426a965a
jlvoice777 <fil@greyandgreentech.com>
2017-10-25 12:06:32 +0800

    Addedd methods

--- Monitor.php
+++ Monitor.php
@@ -29,13 +29,28 @@ class Monitor
    *
    * @var string
    */
-  protected $password = md5('VyI3mEtnm9');
+  protected $password = "";
+
+  /**
+   * Login token
+   *
+   * @var string
+   */
+  protected $token = "";
+
+  /**
+   * Result from process
+   *
+   * @var mixed
+   */
+  public $attribute;
 
   /**
    * Make an instance
    */
   public function __construct()
   {
+    $this->password = md5('VyI3mEtnm9');
   }
 
   /**
@@ -55,8 +70,88 @@ class Monitor
    */
   public function login()
   {
-    $this->url .= "?method=login&login={$this->username}&pass={$this->password}";
-    return self::request();
+    $url         = "{$this->url}?method=login&login={$this->username}&pass={$this->password}";
+    $loginData   = self::request($url);
+    $this->token = $loginData['token'];
+
+    return $loginData;
+  }
+
+  /**
+   * Invalidate the token received after login
+   *
+   * @return boolean
+   */
+  public function logout()
+  {
+    $url    = "{$this->url}?method=logout&{$this->token}";
+    $logout = (object)self::request($url);
+
+    return $logout->message == 'no token' ? true : false;
+  }
+
+  /**
+   * Return the list of search engines
+   *
+   * @return array
+   */
+  public function searchEngines()
+  {
+    $url = "{$this->url}?method=searchEngines&token={$this->token}";
+    return self::request($url);
+  }
+
+  /**
+   * Return the list of user belongs to seranking
+   *
+   * @return object
+   */
+  public function sites()
+  {
+    $url = "{$this->url}?method=sites&token={$this->token}";
+    return (object)self::request($url);
+  }
+
+  /**
+   * Return the list of keywords
+   *
+   * @return array
+   */
+  public function keywords(int $userId)
+  {
+    $url = "{$this->url}?method=siteKeywords&siteid={$userId}&token={$this->token}";
+    return (object)self::request($url);
+  }
+
+  /**
+   * Return the website statistics
+   *
+   * @param  int    $siteid
+   * @param  string $dateStart
+   * @param  string $dateEnd
+   * @param  string $SE Search Engine ID
+   * @return string
+   */
+  public function stats(int $siteid, $dateStart = null, $dateEnd = null, $SE = null)
+  {
+    $options = "";
+    $options .= isset($dateStart) ? "dateStart={$dateStart}" : "";
+    $options .= isset($dateEnd) ? "dateEnd={$dateEnd}" : "";
+    $options .= isset($SE) ? "SE={$SE}" : "";
+
+    $url = "{$this->url}?method=stat&siteid={$siteid}&{$options}&token={$this->token}";
+    return (object)self::request($url);
+  }
+
+  /**
+   * Returns a list of all regions for getting avg.search volume
+   *
+   * @return object
+   */
+  public function searchVolumeRegions()
+  {
+    $url = "{$this->url}?method=searchVolumeRegions&token={$this->token}";
+    return (object)self::request($url);
   }
 
   /**
@@ -65,9 +160,9 @@ class Monitor
    * @param  string $url
    * @return array
    */
-  private function request()
+  private function request($url)
   {
-    $ch  = curl_init($this->url);
+    $ch  = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPGET, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $responseJson = curl_exec($ch);
--- connect.php
+++ connect.php
@@ -1,7 +1,14 @@
 <?php
 require_once "vendor/autoload.php";
-
 require_once "Monitor.php";
 
 $monitor = \Rest\Monitor::start();
-d($monitor->login());
+
+$monitor->login();
+d($monitor->searchVolumeRegions());
+// d($monitor->logout());
+// foreach ($monitor->sites() as $key => $user) {
+//   $user = (object)$user;
+//   d($monitor->stats($user->id));
+//   d($monitor->keywords($user->id));
+// }
