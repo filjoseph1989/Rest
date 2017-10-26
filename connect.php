c0df91e15ef422a57cf37030c228e2d094573f7b
jlvoice777 <fil@greyandgreentech.com>
2017-10-26 12:32:18 +0800

    Added database library

--- connect.php
+++ connect.php
@@ -1,14 +1,30 @@
 <?php
+
+require_once "pdohelper/library/db.php";
 require_once "vendor/autoload.php";
 require_once "Monitor.php";
 
 $monitor = \Rest\Monitor::start();
+$databse = new \Library\DB;
 
-$monitor->login();
-d($monitor->searchVolumeRegions());
-// d($monitor->logout());
-// foreach ($monitor->sites() as $key => $user) {
-//   $user = (object)$user;
-//   d($monitor->stats($user->id));
-//   d($monitor->keywords($user->id));
-// }
+$databse->Connect('mysql', 'monitorbiz', 'root', '', 'localhost');
+$data = [
+	'site_id'                => "1",
+	'site_group_id'          => "2",
+	'website_group_id'       => "3",
+	'name'                   => "sample",
+	'title'                  => "sample title",
+	'today_avg_position'     => "1",
+	'yesterday_avg_position' => "2",
+	'total_up'               => "5",
+	'total_down'             => "6",
+	'keys_count'             => "7",
+	'process'                => "1",
+];
+$databse->Insert('sites', $data, $query = '');
+// d($databse->db); exit;
+
+// $monitor->login();
+//
+// d($monitor->searchEngines());
+// d($monitor->sites());
