cfd4d30849dc15270ffe5689b9f2e763d66955ec
Fil <filjoseph22@gmail.com>
2017-10-31 11:17:43 +0800

    Updated dbscripts

--- dbscripts/monitorbiz.sql
+++ dbscripts/monitorbiz.sql
@@ -19,17 +19,14 @@ USE `monitorbiz`;
 -- Dumping structure for table monitorbiz.search_engines
 CREATE TABLE IF NOT EXISTS `search_engines` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `search_engine_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'This is the ID from the  API',
-  `regoin_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `reference_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'This is the ID from the  API',
+  `reference_region_id` int(11) unsigned DEFAULT '0',
+  `region_id` int(11) unsigned DEFAULT '0',
   `name` char(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   KEY `Index 1` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='The list of search engines';
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='The list of search engines';
 
--- Dumping data for table monitorbiz.search_engines: ~0 rows (approximately)
-DELETE FROM `search_engines`;
-/*!40000 ALTER TABLE `search_engines` DISABLE KEYS */;
-/*!40000 ALTER TABLE `search_engines` ENABLE KEYS */;
-
+-- Data exporting was unselected.
 -- Dumping structure for table monitorbiz.search_engine_region
 CREATE TABLE IF NOT EXISTS `search_engine_region` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -41,11 +38,7 @@ CREATE TABLE IF NOT EXISTS `search_engine_region` (
   CONSTRAINT `FK_search_engine_region_search_engines` FOREIGN KEY (`search_engine_id`) REFERENCES `search_engines` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='For each  search engine there is none or many regoins';
 
--- Dumping data for table monitorbiz.search_engine_region: ~0 rows (approximately)
-DELETE FROM `search_engine_region`;
-/*!40000 ALTER TABLE `search_engine_region` DISABLE KEYS */;
-/*!40000 ALTER TABLE `search_engine_region` ENABLE KEYS */;
-
+-- Data exporting was unselected.
 -- Dumping structure for table monitorbiz.sites
 CREATE TABLE IF NOT EXISTS `sites` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -60,13 +53,9 @@ CREATE TABLE IF NOT EXISTS `sites` (
   `keys_count` int(11) unsigned DEFAULT NULL,
   `process` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='The list of sites';
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='The list of sites';
 
--- Dumping data for table monitorbiz.sites: ~0 rows (approximately)
-DELETE FROM `sites`;
-/*!40000 ALTER TABLE `sites` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sites` ENABLE KEYS */;
-
+-- Data exporting was unselected.
 -- Dumping structure for table monitorbiz.site_search_engines
 CREATE TABLE IF NOT EXISTS `site_search_engines` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -83,11 +72,7 @@ CREATE TABLE IF NOT EXISTS `site_search_engines` (
   CONSTRAINT `FK_site_search_engines_sites` FOREIGN KEY (`site_id`) REFERENCES `sites` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='This table means the for each sites it is attached to one or more search engines';
 
--- Dumping data for table monitorbiz.site_search_engines: ~0 rows (approximately)
-DELETE FROM `site_search_engines`;
-/*!40000 ALTER TABLE `site_search_engines` DISABLE KEYS */;
-/*!40000 ALTER TABLE `site_search_engines` ENABLE KEYS */;
-
+-- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
