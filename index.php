9e2453e0eaef0e72d4f2d11634f01bf6be75cc34
Fil <filjoseph22@gmail.com>
2017-10-31 11:18:18 +0800

    Updated index

--- index.php
+++ index.php
@@ -1,23 +1,60 @@
 <?php
 require_once "vendor/autoload.php";
-// require_once "config.php";
+require_once "pdohelper/library/db.php";
 require_once "helpers/functions.php";
-require_once "Monitor.php";
+require_once "monitor/Monitor.php";
+require_once "monitor/SearchEngine.php";
 
+/**
+ * API request
+ * @var $monitor
+ */
+$monitor = new \Rest\Monitor\Monitor;
+$monitor->login();
+
+/**
+ * configuration
+ * @var Dotenv
+ */
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-d(getenv('APP_NAME'));
-d($_ENV);
-d($_SERVER);
-// $monitor = \Rest\Monitor::start();
-// $monitor->login();
-//
+# Load database
+$database = new \Library\DB;
+
+# Connection
+$database->Connect(
+  getenv('DB_CONNECTION'),
+  getenv('DB_DATABASE'),
+  getenv('DB_USERNAME'),
+  getenv('DB_PASSWORD'),
+  getenv('DB_HOST')
+);
+
+$se     = new \Rest\Monitor\SearchEngine($monitor, $database);
+$result = $se->store();
+d($result);
+exit;
+
+$regions = $otherRegions[0];
+array_shift($otherRegions);
+
+if (is_array($otherRegions)) {
+  foreach ($otherRegions as $key => $otherRegion) {
+    if ($otherRegion === $regions) {
+      echo "yes";
+    } else {
+      echo "Update me, since you have unequal array";
+    }
+  }
+}
+
+exit;
+
 // $data = getSites($monitor->sites());
-//
 // extract($data);
-// // d($sites, $search_engines);
-//
+// d($sites, $search_engines);
+
 // foreach ($sites as $key => $site) {
 // 	$result = $databse->Insert('sites', $site);
 // 	d($result);
